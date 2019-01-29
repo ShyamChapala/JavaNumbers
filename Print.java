beb4be70dbcca440894484ed4888697805f4b64e
Syama Chapala <Syama.Chapala@csscorp.com>
2019-01-29 08:41:53 +0530

    Comment Changes

--- Print.java
+++ Print.java
@@ -3,7 +3,7 @@ public class Print
 	public static void main(String[] args)
 	{
 		//print the result
-		System.out.println("Output is Below: ");
+		System.out.println("Output is: ");
 
 		//loop  to print 1 to 10.
 		for(int i = 1; i <= 10; i++)
